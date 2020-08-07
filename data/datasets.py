353a23aac42081a7b46dbac8987789f9429b8f74
Stefano-Dell <s.medagli@gmail.com>
2020-08-08 00:03:28 +0200

    feat: change paths as string to paths as variable

--- data/datasets.py
+++ data/datasets.py
@@ -4,6 +4,10 @@ import os
 from sklearn.datasets import load_files
 from keras.utils import np_utils
 
+from dog_breed.common import paths
+
+folders = paths.Folders()
+
 
 def load_dataset(path: str):
     """
@@ -17,11 +21,11 @@ def load_dataset(path: str):
     return dog_files, dog_targets
 
 
-def load_training(): return load_dataset('data/dogImages/train')
-def load_test(): return load_dataset('data/dogImages/test')
-def load_validation(): return load_dataset('data/dogImages/valid')
+def load_training(training_folder=folders.training_data): return load_dataset(training_folder)
+def load_test(test_folder=folders.test_data): return load_dataset(test_folder)
+def load_validation(valid_folder=folders.validation_data): return load_dataset(valid_folder)
 
 
-def get_dog_names() -> list:
-    """ Return the list of dog breeds """
-    return list(map(lambda x: x.split('.')[-1], os.listdir('data/dogImages/train/')))
+def get_dog_names(training_folder=folders.training_data) -> list:
+    """ Return the list of dog breeds (from training) """
+    return list(map(lambda x: x.split('.')[-1], os.listdir(training_folder)))
