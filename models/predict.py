4f075ac7270d2049fcfb9dcebcd556cf1befabe6
Stefano-Dell <s.medagli@gmail.com>
2020-08-08 00:02:03 +0200

    feat: add data augmentation

--- models/predict.py
+++ models/predict.py
@@ -4,15 +4,26 @@ import os
 import numpy as np
 
 from keras.callbacks import ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
 
 from dog_breed.models.bottleneck_features import extract_bottleneck_features
 from dog_breed.data.datasets import get_dog_names
 
+args_data_augmentation = {'width_shift_range': 0.3,
+                          'height_shift_range': 0.3,
+                          'horizontal_flip': True,
+                          'rescale': True,
+                          'rotation_range': 0.5,
+                          }
+
+args_model_training = {'epochs': 20,
+                       'verbose': 1,
+                       }
+BATCH_SIZE = 20
+
 
 def load_bottleneck_features(network: str) -> dict:
     """ Loads the precomputed bottleneck features for a series of architectures
-    @ TODO:
-        save the bottleneck features in the 'data' folder
     Args:
         network: kind of network. Can be:
             * VGG19
@@ -34,7 +45,8 @@ def load_network_weights(network, weight_file: str):
 
 
 def train_network(network, bottleneck_network, training_data, training_target,
-                  validation_data, validation_target, overwrite=0, prefix='mynet'):
+                  validation_data, validation_target, overwrite=0, prefix='mynet',
+                  data_augmentation=True):
 
     """ Trains the given network and saves the best weights
     Trains the network using the bottleneck features
@@ -47,25 +59,39 @@ def train_network(network, bottleneck_network, training_data, training_target,
         validation_target:
         overwrite: if 1, overwrites the saved weights
         prefix: prefix of the filename to save weights
+        data_augmentation: if 1 uses data augmentation for training data
     Returns:
     """
-    model_weight_file = f'saved_models/{prefix}_weight.best.{bottleneck_network}.hdf5'
+    model_weight_file = f'saved_models/{prefix}_weight.best.{bottleneck_network}.hdf5' \
+        if data_augmentation == True else f'saved_models/{prefix}_A_weight.best.{bottleneck_network}.hdf5'
     if os.path.exists(model_weight_file) and not overwrite:
         print("Loading existing weights")
     else:
         checkpointer = ModelCheckpoint(filepath=model_weight_file,
                                        verbose=1, save_best_only=True)
-
-        network.fit(training_data, training_target,
-                    validation_data=(validation_data, validation_target),
-                    epochs=10, batch_size=20, callbacks=[checkpointer], verbose=1)
+        if not data_augmentation:
+            # model_weight_file = f'saved_models/{prefix}_weight.best.{bottleneck_network}.hdf5'
+            hist = network.fit(training_data, training_target,
+                               validation_data=(validation_data, validation_target),
+                               callbacks=[checkpointer],
+                               **args_model_training, batch_size=BATCH_SIZE,
+                               )
+        else:
+            # model_weight_file = f'saved_models/{prefix}_A_weight.best.{bottleneck_network}.hdf5'
+            datagen = ImageDataGenerator(**args_data_augmentation)
+            datagen.fit(training_data)
+            hist = network.fit_generator(datagen.flow(training_data, training_target, batch_size=BATCH_SIZE),
+                                         steps_per_epoch=training_data.shape[0] / BATCH_SIZE,
+                                         validation_data=(validation_data, validation_target),
+                                         callbacks=[checkpointer],
+                                         **args_model_training,
+                                         )
     load_network_weights(network, model_weight_file)
+    return hist
 
 
 def predict(network, image_path: str):
     """
-    TODO:
-        implement dictionary 'dog_names'
     Args:
         network:
         image_path:
