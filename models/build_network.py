efc7f81d84417767b99ff5d0ecefab8f34b6bb66
medagli <Stefano.Medagli@rohde-schwarz.com>
2020-08-07 17:41:41 +0200

    feat: add module to build the NN to predict dog breeds

---
+++ models/build_network.py
@@ -0,0 +1,25 @@
+"""
+This module is used to define the network.
+"""
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
+
+
+def build_network(input_shape: tuple, n_of_classes: int) -> Sequential:
+    """ Returns a neural network to
+
+    Args:
+        input_shape:
+        n_of_classes:
+
+    Returns:
+
+    """
+    net = Sequential()
+    net.add(Flatten(input_shape=input_shape))
+    net.add(Dense(512, activation='relu'))
+    net.add(Dropout(0.2))
+    net.add(Dense(n_of_classes, activation='softmax'))
+
+    net.compile(metrics=['accuracy'], loss='categorical_crossentropy', optimizer='rmsprop')
+    return net
