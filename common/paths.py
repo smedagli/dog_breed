8eae7e985f5b6e54ef8c48fec6cd6c5ea70af750
Stefano-Dell <s.medagli@gmail.com>
2020-08-08 00:02:59 +0200

    feat: add paths for dog images

--- common/paths.py
+++ common/paths.py
@@ -7,3 +7,6 @@ class Folders():
     def __init__(self):
         self.data = 'data/'
         self.models = 'saved_models/'
+        self.training_data = os.path.join(self.data, 'dogImages', 'train')
+        self.test_data = os.path.join(self.data, 'dogImages', 'test')
+        self.validation_data = os.path.join(self.data, 'dogImages', 'valid')
