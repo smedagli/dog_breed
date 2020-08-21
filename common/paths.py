cb917027ad5c003a089a7d15b30f5b6f1f2d1758
Stefano-Dell <s.medagli@gmail.com>
2020-08-21 08:37:24 +0200

    feat: redefine paths using the module path as base

--- common/paths.py
+++ common/paths.py
@@ -2,11 +2,12 @@ import os
 
 def listdir(folder: str) -> list: return [os.path.join(folder, file) for file in os.listdir(folder)]
 
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 class Folders():
     def __init__(self):
-        self.data = 'data/'
-        self.models = 'saved_models/'
+        self.data = os.path.join(module_path, 'data')
+        self.models = os.path.join(module_path, 'saved_models')
         self.training_data = os.path.join(self.data, 'dogImages', 'train')
         self.test_data = os.path.join(self.data, 'dogImages', 'test')
         self.validation_data = os.path.join(self.data, 'dogImages', 'valid')
