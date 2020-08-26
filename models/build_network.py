fa2ba51377826d4715b9dda26546d637bea854bb
smedagli <stefano.medagli@rohde-schwarz.com>
2020-08-26 23:33:30 +0200

    feat: change model architecture

--- models/build_network.py
+++ models/build_network.py
@@ -2,7 +2,7 @@
 This module is used to define the network.
 """
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
 
 
 def build_network(input_shape: tuple, n_of_classes: int) -> Sequential:
@@ -16,9 +16,10 @@ def build_network(input_shape: tuple, n_of_classes: int) -> Sequential:
 
     """
     net = Sequential()
-    net.add(Flatten(input_shape=input_shape))
+    # net.add(Flatten(input_shape=input_shape))
+    net.add(GlobalAveragePooling2D(input_shape=input_shape))
     net.add(Dense(512, activation='relu'))
-    net.add(Dropout(0.2))
+    # net.add(Dropout(0.2))
     net.add(Dense(n_of_classes, activation='softmax'))
 
     net.compile(metrics=['accuracy'], loss='categorical_crossentropy', optimizer='rmsprop')
