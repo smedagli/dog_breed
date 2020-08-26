e520d69a1b6059f1de89dc7ef30f6e3362a05c3e
smedagli <stefano.medagli@rohde-schwarz.com>
2020-08-26 23:30:25 +0200

    feat: when loading the image, the image size is given as variable

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -10,7 +10,7 @@ img_size = 224  # images loaded must be resized to this dimension (squared)
 
 def path_to_tensor(img_path):
     # loads RGB image as PIL.Image.Image type
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(img_size, img_size))
     # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
     x = image.img_to_array(img)
     # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
