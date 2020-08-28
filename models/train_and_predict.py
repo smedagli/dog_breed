c087353a8a6b8f6a2210f8fd0e63f0eacce1f140
smedagli <stefano.medagli@rohde-schwarz.com>
2020-08-29 01:50:20 +0200

    feat: add function to train the CNN from the sketch

--- models/train_and_predict.py
+++ models/train_and_predict.py
@@ -1,6 +1,6 @@
 """
 @ TODO:
-    - save history from train_network()
+    - save history from train_network_tl()
 """
 import io
 import os
@@ -45,10 +45,10 @@ def load_network_weights(network, weight_file: str):
     network.load_weights(weight_file)
 
 
-def train_network(network, bottleneck_network: str, training_data, training_target,
-                  validation_data, validation_target, overwrite=0, prefix='mynet',
-                  data_augmentation=True, epochs=15, batch_size=20):
-    """ Trains the given network and saves the best weights (and history)
+def train_network_tl(network, bottleneck_network: str, training_data, training_target,
+                     validation_data, validation_target, overwrite=0, prefix='mynet',
+                     data_augmentation=True, epochs=15, batch_size=20):
+    """ Trains the given network and saves the best weights (and history) - Transfer Learning
     Args:
         network:
         bottleneck_network:
@@ -69,7 +69,7 @@ def train_network(network, bottleneck_network: str, training_data, training_targ
                            }
 
     model_weight_file = paths.get_weights_filename(bottleneck_network, prefix, epochs, data_augmentation)
-    model_hist_file = paths.get_hist_filename(bottleneck_network, prefix, epochs, data_augmentation)
+    # model_hist_file = paths.get_hist_filename(bottleneck_network, prefix, epochs, data_augmentation)
 
     if os.path.exists(model_weight_file) and not overwrite:
         print("Loading existing weights")
@@ -78,29 +78,21 @@ def train_network(network, bottleneck_network: str, training_data, training_targ
         checkpointer = ModelCheckpoint(filepath=model_weight_file,
                                        verbose=1, save_best_only=True)
         if not data_augmentation:
-            # model_weight_file = f'saved_models/{prefix}_weight.best.{bottleneck_network}.hdf5'
             hist = network.fit(training_data, training_target,
                                validation_data=(validation_data, validation_target),
                                callbacks=[checkpointer],
                                **args_model_training, batch_size=batch_size,
                                )
         else:
-            # model_weight_file = f'saved_models/{prefix}_A_weight.best.{bottleneck_network}.hdf5'
             datagen = ImageDataGenerator(**args_data_augmentation)
             datagen.fit(training_data)
-            # hist = network.fit_generator(datagen.flow(training_data, training_target, batch_size=batch_size),
-            #                              steps_per_epoch=training_data.shape[0] / batch_size,
-            #                              validation_data=(validation_data, validation_target),
-            #                              callbacks=[checkpointer],
-            #                              **args_model_training,
-            #                              )
             hist = network.fit(datagen.flow(training_data, training_target, batch_size=batch_size),
                                steps_per_epoch=training_data.shape[0] / batch_size,
                                validation_data=(validation_data, validation_target),
                                callbacks=[checkpointer],
                                **args_model_training,
                                )
-        pickle.dump(hist.history, open(model_hist_file, 'wb'))
+        # pickle.dump(hist.history, open(model_hist_file, 'wb'))
 
     load_network_weights(network, model_weight_file)
     # return hist
@@ -119,3 +111,41 @@ def predict(network, image_path: str) -> str:
     bottleneck_features = extract_bottleneck_features(network, image_path)
     pred = network.predict(bottleneck_features)
     return dog_names[int(np.argmax(pred))]
+
+
+def train_cnn(cnn, training_data, training_target,
+              validation_data, validation_target, overwrite=0, prefix='CNN',
+              data_augmentation=True, epochs=15, batch_size=20):
+
+    args_model_training = {'epochs': epochs,
+                           'verbose': 1,
+                           }
+
+    model_weight_file = paths.get_weights_filename('', prefix, epochs, data_augmentation,
+                                                   transfer_learning=False).replace('..', '.')
+    # model_hist_file = paths.get_hist_filename('', prefix, epochs, data_augmentation).replace('..', '.')
+
+    if os.path.exists(model_weight_file) and not overwrite:
+        print("Loading existing weights")
+        # hist = pickle.load(open(model_hist_file, 'rb'))
+    else:
+        checkpointer = ModelCheckpoint(filepath=model_weight_file,
+                                       verbose=1, save_best_only=True)
+        if not data_augmentation:
+            # model_weight_file = f'saved_models/{prefix}_weight.best.{bottleneck_network}.hdf5'
+            cnn.fit(training_data, training_target,
+                    validation_data=(validation_data, validation_target),
+                    callbacks=[checkpointer],
+                    **args_model_training, batch_size=batch_size,
+                    )
+        else:
+            datagen = ImageDataGenerator(**args_data_augmentation)
+            datagen.fit(training_data)
+            cnn.fit(datagen.flow(training_data, training_target, batch_size=batch_size),
+                    steps_per_epoch=training_data.shape[0] / batch_size,
+                    validation_data=(validation_data, validation_target),
+                    callbacks=[checkpointer],
+                    **args_model_training,
+                    )
+
+    load_network_weights(cnn, model_weight_file)
