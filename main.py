4bdd583fb326fa772703b4461817f5723168c32d
smedagli <stefano.medagli@rohde-schwarz.com>
2020-08-28 22:18:56 +0200

    feat: modify main. Now can be used to train multiple models.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
-This module uses Transfer Learning to classify the breed of dogs from the picture.
+This module uses Transfer Learning to train multiple models.
+The parameters are defined as factorial combination of NET, EPOCHS and DATA_AUGMENTATION.
+
 Procedure:
     * bottle neck features are computed from state-of-the-art networks
         * VGG16
@@ -12,91 +14,64 @@ from PIL import ImageFile
 import pickle
 import os
 import numpy as np
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+from itertools import product
 
 from dog_breed.models import build_network as bn
 from dog_breed.data import datasets
 from dog_breed.preprocessing import preprocess
-from dog_breed.models import predict
+from dog_breed.models import trainAndPredict
 from dog_breed.models import bottleneck_features as bf
 from dog_breed.common import tools as ct
 from dog_breed.common import metrics
 
-# get all file names and labels
-train_files, y_train = datasets.load_training()
-valid_files, y_valid = datasets.load_validation()
-test_files, y_test = datasets.load_test()
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-soa_network = 'InceptionV3'
-for soa_network in ['VGG16', 'VGG19', 'Resnet50', 'InceptionV3', 'Xception']:
+NETS = ['vgg16', 'vgg19', 'resnet50', 'inceptionv3', 'xception']
+EPOCHS = [5, 10, 20]
+DATA_AUGMENTATION = [False, True]
 
+if __name__ == '__main__':
+    # get all file names and labels
+    train_files, y_train = datasets.load_training()
+    valid_files, y_valid = datasets.load_validation()
+    test_files, y_test = datasets.load_test()
+    n_classes = len(datasets.get_dog_names())
     # compute the tensors
-    tensors_file = f'data/tensors.npy'
-    if not os.path.exists(tensors_file):
-        # tensors_train = preprocess.paths_to_tensor(train_files).astype('float32') / 255
-        # tensors_valid = preprocess.paths_to_tensor(valid_files).astype('float32') / 255
-        # tensors_test = preprocess.paths_to_tensor(test_files).astype('float32') / 255
+    # tensors_train = preprocess.paths_to_tensor(train_files).astype('float32') / 255
+    # tensors_valid = preprocess.paths_to_tensor(valid_files).astype('float32') / 255
+    # tensors_test = preprocess.paths_to_tensor(test_files).astype('float32') / 255
+    tensors_train = list(map(lambda x: preprocess.path_to_tensor(x).astype('float32') / 255, ct.progr(train_files)))
+    tensors_valid = list(map(lambda x: preprocess.path_to_tensor(x).astype('float32') / 255, ct.progr(valid_files)))
+    tensors_test = list(map(lambda x: preprocess.path_to_tensor(x).astype('float32') / 255, ct.progr(test_files)))
 
-        tensors_train = list(map(lambda x: preprocess.path_to_tensor(x), ct.progr(train_files)))
-        tensors_valid = list(map(lambda x: preprocess.path_to_tensor(x), ct.progr(valid_files)))
-        tensors_test = list(map(lambda x: preprocess.path_to_tensor(x), ct.progr(test_files)))
-        # np.save(tensors_file, [tensors_train, tensors_valid, tensors_test])
-    # else:
-    #     tensors_train, tensors_valid, tensors_test = np.load(tensors_file, allow_pickle=True)
-    # compute bottleneck features
-    bottle_file = f'data/bottleneck_features/bottleneck_{soa_network}.pkl'
-    if not os.path.exists(bottle_file):
-        bottleneck_train = bf.extract_bottleneck_features_list(soa_network, tensors_train)
-        bottleneck_valid = bf.extract_bottleneck_features_list(soa_network, tensors_valid)
-        bottleneck_test = bf.extract_bottleneck_features_list(soa_network, tensors_test)
-        pickle.dump([bottleneck_train, bottleneck_valid, bottleneck_test], open(bottle_file, 'wb'))
-    else:
-        bottleneck_train, bottleneck_valid, bottleneck_test = pickle.load(open(bottle_file, 'rb'))
+    for soa_network in reversed(NETS):
+        # compute bottleneck features
+        bottle_file = f'data/bottleneck_features/bottleneck_{soa_network}.pkl'
+        if not os.path.exists(bottle_file):
+            bottleneck_train = bf.extract_bottleneck_features_list(soa_network, tensors_train)
+            bottleneck_valid = bf.extract_bottleneck_features_list(soa_network, tensors_valid)
+            bottleneck_test = bf.extract_bottleneck_features_list(soa_network, tensors_test)
+            pickle.dump([bottleneck_train, bottleneck_valid, bottleneck_test], open(bottle_file, 'wb'))
+        else:
+            bottleneck_train, bottleneck_valid, bottleneck_test = pickle.load(open(bottle_file, 'rb'))
 
+        for i in product(EPOCHS, DATA_AUGMENTATION):
+            print(soa_network, i)
+            epochs, data_augmentation = i
 
-    dog_names = datasets.get_dog_names()
-    n_classes = len(dog_names)
+            model = bn.build_transfer_learning_netwok(input_shape=bottleneck_train[0].shape, n_of_classes=n_classes)
+            model.summary()
 
-    model = bn.build_network(input_shape=bottleneck_train[0].shape, n_of_classes=n_classes)
-    model.summary()
+            hist = trainAndPredict.train_network(network=model, bottleneck_network=soa_network,
+                                                 training_data=bottleneck_train, training_target=y_train,
+                                                 validation_data=bottleneck_valid, validation_target=y_valid,
+                                                 overwrite=0, prefix='tl',
+                                                 data_augmentation=data_augmentation,
+                                                 epochs=epochs,
+                                                 )
 
-    hist = predict.train_network(network=model, bottleneck_network=soa_network,
-                                 training_data=bottleneck_train, training_target=y_train,
-                                 validation_data=bottleneck_valid, validation_target=y_valid,
-                                 overwrite=1, prefix='mynet',
-                                 data_augmentation=True,
-                                 epochs=25,
-                                 )
-#
-# # prefix = 'mynet'
-# # epochs = 5
-# # bottleneck_network = soa_network
-# # model_weight_file = f'saved_models/{prefix}_{epochs}_A_weight.best.{bottleneck_network}.hdf5'
-# # model.load_weights(model_weight_file)
-# # pred_train = model.predict(bottleneck_train)
-#
-# pred_test = model.predict(bottleneck_test)
-# pred = np.array([np.argmax(x) for x in pred_test])
-#
-# metrics.get_accuracy(np.array([np.argmax(x) for x in pred_test]), np.array([np.argmax(y) for y in y_test]))
-#
-# (np.array([np.argmax(x) for x in pred_test]) == np.array([np.argmax(y) for y in y_test])).sum() / len(y_test) * 100
-#
-# import matplotlib.pyplot as plt
-# h = hist[0]
-# plt.close('all')
-# plt.figure()
-# plt.subplot(211)
-# plt.title('Loss')
-# plt.plot(h.history['val_loss'])
-# plt.plot(h.history['loss'])
-# plt.xticks(range(len(h.history['loss'])), range(1, len(h.history['loss']) + 1))
-# plt.xlabel("Epoch")
-# plt.subplot(212)
-# plt.title('Accuracy')
-# plt.plot(h.history['val_accuracy'])
-# plt.plot(h.history['accuracy'])
-# plt.legend(['validation', 'training'])
-# plt.xticks(range(len(h.history['loss'])), range(1, len(h.history['loss']) + 1))
-# plt.xlabel("Epoch")
-# # # model.fit(bottleneck_train, y_train, )
+            pred_test_probl = model.predict(bottleneck_test)
+            pred_test = np.array([np.argmax(x) for x in pred_test_probl])
+            labels_test = np.array([np.argmax(y) for y in y_test])
+            test_accuracy = metrics.get_accuracy(pred_test, labels_test) * 100
+            print(f'Test accuracy {test_accuracy:3.2f}%')
