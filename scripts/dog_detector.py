cf9d298daf06da57863b335fb23cb62f931b87a2
smedagli <stefano.medagli@rohde-schwarz.com>
2020-08-28 22:19:57 +0200

    docs: add description to dog detector script

--- scripts/dog_detector.py
+++ scripts/dog_detector.py
@@ -1,12 +1,13 @@
 """
-Will print whether or not there is a dog in the input image file
+Will print whether or not there is a dog in the input image file.
 """
 import argparse
 from dog_breed.detectors import detectors
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Will print whether or not there is a dog in the input image file.')
+
     parser.add_argument('-f', '--file', help='Path of the image', required=True)
     args = parser.parse_args()
     dog = detectors.is_dog(args.file)
